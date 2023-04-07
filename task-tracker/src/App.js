c2d6571fa7637401fff6ae5e93df269b544618da
Aaron <soteloaaron16@gmail.com>
2023-04-07 08:43:13 -0500

    added components and deletion of tasks

--- task-tracker/src/App.js
+++ task-tracker/src/App.js
@@ -1,7 +1,37 @@
-function App() {
+import { useState } from 'react' 
+import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+
+const App = () => {
+  const [tasks, setTasks] = useState([
+    {
+        id: 1,
+        text: 'Food shopping',
+        day: 'Apr 8th',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: 'Movie',
+        day: 'Apr 7th at 11:45am',
+        reminder: true,
+    },
+    {
+        id: 3,
+        text: 'Dog shopping',
+        day: 'Apr 7th after movie',
+        reminder: 'false',
+    }
+])
+
+const deleteTask = (id) => {
+  setTasks(tasks.filter((task) => task.id !== id))
+}
+
   return (
-    <div className="App">
-      <h1>Hello From React</h1>
+    <div className="container">
+      <Header />
+      <Tasks tasks={tasks} onDelete={deleteTask} />
     </div>
   );
 }
